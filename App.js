b39d2941621f2975e188c712184a9de9eec848f9
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-02 09:31:30 +0000

    Initial commit, added tailwind

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { t } from 'react-native-tailwindcss';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[t.flex1, t.justifyCenter, t.itemsCenter]}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
