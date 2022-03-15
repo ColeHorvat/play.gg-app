e82f006621dc76aaf4ca4c175d00d763d11ff144
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-15 23:04:26 +0000

    Revert "Added constant checking for steam api"
    This reverts commit 40830a9a30e92170c51faa75f18ab3b38c25df4a.

--- App.js
+++ App.js
@@ -4,44 +4,41 @@ import { t } from 'react-native-tailwindcss';
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-	const API_KEY = '17CB3BD18765C9F04AAB50A3EC9CA2A3';
-	const RB_STEAMID = '76561198114121125';
-	const URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + API_KEY + '&steamids=' + RB_STEAMID;
+  const API_KEY = '17CB3BD18765C9F04AAB50A3EC9CA2A3';
+  const RB_STEAMID = '76561198114121125';
+  const URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + API_KEY + '&steamids=' + RB_STEAMID;
 
-	const [player, setPlayer] = useState([]);
-	const [loading, setLoading] = useState(true)
+  const [player, setPlayer] = useState([]);
+  const [loading, setLoading] = useState(true)
 
-	useEffect(() => {
-		setInterval(() => {
-			fetch(URL)
-			.then((response) => response.json())
-			.then((responseJson) => {
-				return responseJson['response']['players']['0'];
-			})
-			.then(player => {
-				setPlayer(player);
-				setLoading(false);
-			})
-			.catch(error => {
-				console.error(error);
-			})
-		}, 1000)
-	})
+  useEffect(() => {
+    fetch(URL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson['response']['players']['0'];
+    })
+    .then( player => {
+      setPlayer(player);
+      setLoading(false);
+    })
+    .catch( error => {
+      console.error(error);
+    })
+  }, []);
 
-	return (
-		<View style={[t.flex1, t.justifyCenter, t.itemsCenter]}>
-			<Text> {loading ? 'Loading...' : player['personaname'] + " " + player['personastate']} </Text> 
-			<StatusBar style="auto"/>
-		</View>
-	);
+  return (
+    <View style={[t.flex1, t.justifyCenter, t.itemsCenter]}>
+      <Text>{ loading ? 'Loading...' : player['personaname']}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
