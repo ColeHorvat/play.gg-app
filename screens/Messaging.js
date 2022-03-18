7b72ddd856e30e8bedd50c098b13192d9137a53b
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-18 03:05:45 +0000

    Finished header and added icon buttons

--- screens/Messaging.js
+++ screens/Messaging.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, Pressable, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { t } from 'react-native-tailwindcss';
 import Constants from 'expo-constants'
+import Icon from 'react-native-vector-icons/Feather'
 
 const Messaging = ({ navigation }) => {
 	return (
@@ -10,14 +11,43 @@ const Messaging = ({ navigation }) => {
 			<StatusBar style="light" />
 			{/* HEADER */}
 			<View style={styles.header}>
-				<Image />
-				<Text>SirPancakes</Text>
-				<Text>Playing: Halo Infinite</Text>
+				<View style={styles.iconContainer}>
+					<Image 
+						source={require('../assets/images/playgg_logo_image_only.png')}
+						style={ styles.icon }	 
+					/>
+				</View>
+				<View>
+					<Text style={ styles.headerNameText }>SirPancakes</Text>
+					<Text>
+						<Text style={ styles.headerPlayingText }>Playing:</Text>
+						{/* IMAGE HERE */} 
+						<Text style={ styles.headerGameText }> Halo Infinite</Text>
+					</Text>
+				</View>
 			</View>
 
 			<ScrollView>
-
+				{ /* MESSAGES */ } 
 			</ScrollView>
+
+			<View styles={ styles.inputContainer }>
+				<TextInput
+					style={ styles.inputText }
+				/>
+				<Button
+					title=""
+				/>
+				<Icon.Button 
+					name="send"
+					color="#D92344"
+					backgroundColor="#73172F"
+					iconStyle={ styles.messageButtons }
+
+				>
+
+				</Icon.Button>
+			</View>
 		</View>
 	)
 }
@@ -49,8 +79,49 @@ const styles = StyleSheet.create({
 		height: '10%',
 		width: '100%',
 		backgroundColor: '#73172F',
-		marginTop: Constants.statusBarHeight
+		marginTop: Constants.statusBarHeight,
+		flexDirection: 'row',
 	},
+	icon: {
+		maxWidth: 75,
+		maxHeight: 75
+	},
+	iconContainer: {
+		height: '100%',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	headerNameText: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#D92344',
+		marginTop: 4,
+		marginBottom: 4
+	},
+	headerPlayingText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: '#076600'
+	},
+	headerGameText: {
+		fontSize: 18,
+		fontWeight: 'bold',
+		color: '#FFFFFF'
+	},
+	inputContainer: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+	},
+	messageInput: {
+		height: 40,
+		margin: 12,
+		borderWidth: 1,
+		padding: 10,
+	},
+	messageButtons: {
+		
+	}
 });
 
 export default Messaging
