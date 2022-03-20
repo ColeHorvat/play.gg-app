49a615be33c7e5f9105f6cf530bd7c9f4607eb6d
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-20 14:13:35 +0000

    Added date and time picker

--- screens/Messaging.js
+++ screens/Messaging.js
@@ -1,9 +1,9 @@
 import React, { useState, useRef } from 'react'
-import { StyleSheet, Text, View, Image, ScrollView, TextInput, Pressable, Button } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { t } from 'react-native-tailwindcss';
 import Constants from 'expo-constants'
 import Icon from 'react-native-vector-icons/Feather'
+import DateTimePicker from '@react-native-community/datetimepicker'
 
 { /* MESSAGE DATA */}
 
@@ -26,8 +26,20 @@ const Messaging = ({ navigation }) => {
 
 	const [messageData, setMessageData] = useState([]);
 	const [messageText, setMessageText] = useState('');
+	const [inviteActive, setInviteActive] = useState(true);
+	const [addedFriends, setAddedFriends] = useState([]);
+
+	const [date, setDate] = useState(new Date(1598051730000));
+	const [mode, setMode] = useState('date');
+	const [show, setShow] = useState(false);
+
+	
 	const scrollView = useRef(null);
 	const textInput = useRef(null);
+
+	const FRIENDS = {
+		"1":"SirPancakes"
+	}
 
 	const messages = messageData.map((message) => {
 		if(message.method === 'send') {
@@ -38,7 +50,8 @@ const Messaging = ({ navigation }) => {
 	})
 
 	return (
-		<View style={ styles.container }>
+		<View style={ [ styles.container ]}>
+			{ console.log(show) }
 			<StatusBar style="light" />
 			{/* HEADER */}
 			<View style={styles.header}>
@@ -84,7 +97,7 @@ const Messaging = ({ navigation }) => {
 						name="inbox"
 						color="#D92344"
 						backgroundColor="#211626"
-						onPress={() => { console.log("INVITE") }}
+						onPress={ toggleInvite }
 					>
 
 					</Icon.Button>
@@ -102,7 +115,13 @@ const Messaging = ({ navigation }) => {
 					</Icon.Button>
 				</View>
 
+
 			</View>
+
+			{inviteActive && (
+
+				<InviteCard />
+			)}
 		</View>
 	)
 
@@ -124,6 +143,168 @@ const Messaging = ({ navigation }) => {
 		)
 	}
 
+	{ /* INVITE COMPONENTS */}
+
+	function InviteCard(props) {
+
+		// DATE PICKER STATES & FUNCTIONS
+
+
+
+		const onChange = (event, selectedDate) => {
+			const currentDate = selectedDate || date;
+			setShow(Platform.ios);
+			setDate(currentDate);
+		}
+
+		const showMode = (currentMode) => {
+			setShow(true)
+			setMode(currentMode)
+		}
+
+		const showDatepicker = () => { 
+			showMode('date');
+		}
+
+		const showTimepicker = () => {
+			showMode('time');
+		}
+
+
+		return (
+			<View style = { [styles.inviteContainer] }>
+				<View style = { [styles.header, { backgroundColor: '#2B222E', alignItems: 'center', justifyContent: 'center'}]}>
+					<View style = { [{ alignSelf: 'flex-start', position: 'absolute', left: 0}]}>
+						<Icon.Button 
+							name="x"
+							style={ [styles.icon, { alignSelf: 'flex-start'}] }
+							size={30}	 
+							color='white'
+							backgroundColor='#2B222E'
+							onPress={ toggleInvite }
+						/>
+					</View>
+									
+
+					<Text style ={[ styles.headerNameText, {} ]}>Create An Invite</Text>
+				</View>
+
+				<View style={[ {marginBottom: 20}]}>
+					<TextInput 
+						style={ [styles.inviteMessageInput, { marginTop: 12}]}
+						placeholder="Title"
+						placeholderTextColor='white'
+					/>
+
+					<TextInput
+						onPressIn={ showDatepicker }
+						style={ [styles.inviteMessageInput]}
+						placeholder='Date'
+						placeholderTextColor='white'
+					/>
+				</View>
+
+				<View>
+					<View style={ [ { flexDirection: 'row' } ]}>
+						<Text style={ [ {marginTop: 18, fontSize: 18, color: 'white', marginLeft: 10 }]}> Start:</Text>
+						<TextInput
+							onPressIn={ showTimepicker } 
+							style={[styles.inviteMessageInput]}
+						/>
+					</View>
+				
+					<View style={ { flexDirection: 'row'} }>
+						<Text style={ [ { marginTop: 18, fontSize: 18, color: 'white', marginLeft: 10} ]}> End:</Text>
+						<TextInput
+							onPressIn={ showTimepicker } 
+							style={[styles.inviteMessageInput]}
+						/>
+					</View>
+
+					{show && (
+						<DateTimePicker 
+							testID="dateTimePicker"
+							value={date}
+							mode={mode}
+							is24Hour={true}
+							display="default"
+							onChange={onChange}
+						/>
+					)}
+
+					{ /* SELECT FRIEND MENU */}
+					
+				</View>
+				
+				<View style={ [styles.inviteSendButton, { marginTop: 24, marginLeft: 10, flexDirection: 'row'}]} >	
+						{/* <Icon 
+							name="user"
+							style={ [styles.inviteSendButton]}
+							size={35}
+							color='white'
+						/> */}
+						{/* <View style={ [{width: 150, height: 200}]}>
+							<CustomerMultiPicker 
+								options={FRIENDS}
+								search={false} // should show search bar?
+								multiple={true} //
+								placeholder={"Friends"}
+								placeholderTextColor={'#757575'}
+								returnValue={"label"} // label or value
+								callback={(res)=> setAddedFriends(res)} // callback, array of selected items
+								rowBackgroundColor={"#eee"}
+								rowHeight={40}
+								rowRadius={5}
+								searchIconName="ios-checkmark"
+								searchIconColor="red"
+								searchIconSize={30}
+								iconColor={"#00a2dd"}
+								iconSize={30}
+								selectedIconName={"ios-checkmark-circle-outline"}
+								unselectedIconName={"ios-radio-button-off-outline"}
+								scrollViewHeight={130}
+							/>
+						</View> */}
+						{/* <RNPickerSelect
+							style={[styles.inviteSendButton]} 
+							onValueChange={(value) => console.log(value)}
+							Icon={() => { return <Icon name="chevron-down" size={30} color="white"/> }}
+							items={[
+								{ label: 'Football', value: 'football' },
+								{ label: 'Baseball', value: 'baseball' },
+								{ label: 'Hockey', value: 'hockey' },
+							]}
+							style={  { iconContainer: {
+								right: -20,
+							}}}
+							placeholder={{label: 'Select a friend...'}}
+						/> */}
+						
+						{/* <OptionsMenu 
+							style={[styles.inviteSendButton, { }]}
+							customButton={ ( <Icon name="chevron-down" size={30} color="white"/>) }
+							options={ FRIENDS }
+							actions={ FRIENDS.map((friend) => { addInviteFriend(friend) })}
+						/> */}
+						<Text> { addedFriends }</Text>
+				</View>
+					
+
+				<View style = { [ styles.inviteSendContainer ] }>
+					<View style = { [ styles.inviteSendButton ]}>
+						<Icon.Button 
+							name="send"
+							color="#D92344"
+							backgroundColor="#211626"
+							onPress={ sendMessage }
+						/>
+					</View>
+				</View>
+				
+			</View>
+		)
+	}
+
 	{ /* UTIL FUNCTIONS */}
 
 	function sendMessage() { 
@@ -131,12 +312,23 @@ const Messaging = ({ navigation }) => {
 			setMessageData([...messageData, { method: 'send', content: messageText }])
 			setMessageText('')
 			textInput.current.clear()
-
 		}
 	}
 
 	function receiveMessage() {
 		setMessageData([...messageData, { method: 'receive', content: 'NEW MESSAGE'}])
+	}
+
+	function toggleInvite() {
+		setInviteActive(!inviteActive)
+	}
+
+	function generateKey(pre) {
+		return `${ pre }_${new Date().getTime() }`
+	}
+
+	function addInviteFriend(friendName) {
+		setAddedFriends(...addedFriends, generatefriendName )
 	}
 }
 
@@ -200,12 +392,34 @@ const styles = StyleSheet.create({
 		borderRadius: 25,
 		backgroundColor: '#FFFFFF'
 	},
+	inviteMessageInput: {
+		height: 40,
+		width: '70%',
+		marginHorizontal: 12,
+		borderBottomWidth: 1,
+		borderColor: '#707070',
+		paddingHorizontal: 10,
+		backgroundColor: '#211626',
+		color: 'white',
+		fontSize: 18,
+		
+	},
 	messageButtons: {
 		width : 45,
 		height : 45,
 		marginHorizontal: 2,
 		marginBottom: 12,
 
+	},
+	inviteSendContainer: {
+		position: 'absolute',
+		right: 0,
+		bottom: 0,
+		marginHorizontal: 4,
+	},
+	inviteSendButton: {
+		width : 45,
+		height : 45,
 	},
 	item: {
 		marginVertical: 14,
@@ -246,6 +460,14 @@ const styles = StyleSheet.create({
 	arrowRightContainer: {
 		justifyContent: 'flex-end',
 		alignItems: 'flex-end'
+	},
+	inviteContainer: {
+		position: 'absolute',
+		bottom: 0,
+		left: 0,
+		right: 0,
+		backgroundColor: '#211626',
+		height: '50%'
 	}
 });
 
