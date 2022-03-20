fc6714167fb2826928d6e407133a79655aa2085a
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-20 20:51:47 +0000

    Added steam api to cards

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text, View, StyleSheet, Button, Image, TouchableOpacity, ScrollView, Pressabble } from 'react-native';
 import { StatusBar } from 'expo-status-bar'
 import Constants from 'expo-constants';
@@ -7,19 +7,63 @@ import ProfilePicture from 'react-native-profile-picture'
 
 
 const Dashboard = ({ navigation }) => {
+	const API_KEY = '17CB3BD18765C9F04AAB50A3EC9CA2A3'
+	const RB_STEAMID = '76561198114121125';
+	const RB_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + API_KEY + '&steamids=' + RB_STEAMID;
+	const BASE_URL = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + API_KEY + '&steamids=';
 
 	const [show, setShow] = useState(false);
 	const [friends, setFriends] = useState([ 
 		{
 			name: '',
-			status: '',
+			status: null,
 			gamePlaying: '',
-			SteamID: '76561198124794637'
+			SteamID: '76561198124794637',
+			Platform: 'Steam'
 		},
 		{
-
+			name: '',
+			status: null,
+			gamePlaying: '',
+			SteamID: RB_STEAMID,
+			Platform: 'Steam'
 		}
 	]);
+
+	useEffect(() => {
+
+		setInterval(() => {
+			friends.map((friend, i) => {
+				fetch(BASE_URL + friend.SteamID)
+				.then((response) => response.json())
+				.then((responseJson) => {
+					return responseJson['response']['players']['0'];
+				})
+				.then(player => {
+					// friend.name = player['personaname']
+					// friend.status = player['personastate'];
+					// if(player['gameextrainfo'])
+					// 	friend.gamePlaying = player['gameextrainfo']
+
+					const updatedFriends = [...friends];
+					updatedFriends[i].name = player['personaname']
+					updatedFriends[i].status = player['personastate'];
+					if(player['gameextrainfo'])
+						if(player['gameextrainfo'].length < 19)
+							updatedFriends[i].gamePlaying = player['gameextrainfo']
+						else {
+							updatedFriends[i].gamePlaying = player['gameextrainfo'].substring(0, 18) + '...' 
+						}
+					else 
+						updatedFriends[i].gamePlaying = ''
+					setFriends(updatedFriends)
+				})
+				.catch(error => {
+					console.error(error);
+				})
+			})
+		}, 2000)
+	})
 
 	return (
 		<View style={styles.container}>
@@ -55,14 +99,28 @@ const Dashboard = ({ navigation }) => {
 									height={75}
 									requirePicture={require('../assets/avatar.jpg')}
 									isPicture={true}
+									user="SirPancakes"
+									URLPicture="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/76/76d6ea598ba63453a9ae8e953096bd00ce478f55.jpg"
 								/>
 							</View>
 
-							<View>
-								<Text> </Text>
-								<View>
-									<Image />
-									<Text> </Text>
+							<View style={ [styles.header, {justifyContent: 'center', flexDirection: 'column', marginHorizontal: 24, marginBottom: 42}] }>
+								<Text style={ [styles.headerNameText, {color: 'white', fontSize: 24}] }> {friend.name} </Text>
+								<View style={ {flexDirection: 'row', alignItems: 'center'} }>
+									{friend.status >= 1 && (
+										<Image 
+											source={require('../assets/steam_logo.png')}
+											style = { {width: 20, height: 20} }
+										/>
+									)}
+
+									<Text style={ [styles.headerNameText, {color: 'white', fontSize: 16}] }> {friend.gamePlaying != '' ? "Playing: " + friend.gamePlaying : ''} 
+										<Text style={ [styles.headerNameText, {color: 'white', fontSize: 16}]}>
+											{
+												StatusText(friend)
+											}	
+										</Text> 
+									</Text>
 								</View>
 							</View>
 						</View>
@@ -71,9 +129,32 @@ const Dashboard = ({ navigation }) => {
 			</ScrollView>
 		</View>
 	)
+	
+	function StatusText(friend) {
+			if(friend.gamePlaying === '') {
+				switch(friend.status) {
+					case 0:
+						return 'Offline'
+					case 1:
+						return 'Online'
+					case 2:
+						return 'Busy'
+					case 3:
+						return 'Away'
+					case 4: 
+						return 'Snooze'
+					case 5:
+						return 'Online'
+					case 6:
+						return 'Online'
+					default:
+						return 'Loading...'
+				}
+			}
+		}
+	}
 
 
-}
 
 
 const styles = StyleSheet.create({
@@ -85,8 +166,8 @@ const styles = StyleSheet.create({
 
 	header: {
 		flexDirection: "row",
-		justifyContent: "space-between",
 		backgroundColor: '#73172f',
+		justifyContent: 'space-between',
 		marginTop: Constants.statusBarHeight,
 	},
 
@@ -125,6 +206,7 @@ const styles = StyleSheet.create({
 		width: '100%',
 		backgroundColor: '#73172F',
 		marginVertical: 24,
+		flexDirection: 'row',
 	}
 
 });
