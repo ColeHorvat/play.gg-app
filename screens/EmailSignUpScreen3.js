b94bad9ed9bea1a7667f32a1e2965cd051c83f71
phoenixparisian <38114613+phoenixparisian@users.noreply.github.com>
2022-03-20 23:07:43 -0700

    Update EmailSignUpScreen3.js

--- screens/EmailSignUpScreen3.js
+++ screens/EmailSignUpScreen3.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef } from 'react'
 import { TextInput, Button, Image, Text, View, ImageBackground, StyleSheet, Pressable, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants'
@@ -10,6 +10,23 @@ import Icon from 'react-native-vector-icons/Feather'
 const EmailSignUp3 = ({ navigation }) => {
 
   const [text, onChangeText] = React.useState("Text");
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+
+  const checkTextInput = () => {
+    if (password1 != password2) {
+      alert('Please make sure your passwords match.');
+      return;
+    }
+    if(password2.length < 8) {
+      alert('Please enter a password with at least 8 characters')
+      return;
+    }
+    //Move to next page
+    navigation.navigate('EmailSignIn1')
+    
+  };
+
 
   return (
     /* CONTAINER  */
@@ -17,35 +34,48 @@ const EmailSignUp3 = ({ navigation }) => {
       <StatusBar style='light' />
       <ImageBackground source={require('./assets/background_1.png')} resizeMode='stretch' style={styles.backgroundImage}>
 
-        <View style={styles.header}>
-          <Button style={styles.backButton} title="Go back" onPress={() => navigation.navigate('MainLoginScreen')} />
-
-        </View>
 
         <View style={styles.infoMenu}>
           <Text style={styles.buttonText}>Create Password:</Text>
 
           <TextInput
             style={styles.input}
-            onChangeText={onChangeText}
+            onChangeText={value => setPassword1(value)}
             placeholder="Enter your password"
+            textAlign={'center'}
+            autoCapitalize='none'
+            autoComplete="off"
+            autoCorrect='none'
+            selectTextOnFocus={false}
+            underlineColor={'transparent'}
           />
           <Text style={styles.buttonText}>Verify Password</Text>
 
           <TextInput
             style={styles.input}
-            onChangeText={onChangeText}
+            onChangeText={value => setPassword2(value)}
             placeholder="Re-enter your password"
+            textAlign={'center'}
+            autoCapitalize='none'
+            autoComplete="off"
+            autoCorrect='none'
+            selectTextOnFocus={false}
+            underlineColor={'transparent'}
           />
+          <Text style={styles.buttonText}>Passwords must be:</Text>
+          <Text style={styles.buttonText}>at least 8 characters</Text>
+          
 
 
         </View>
 
 
         <View style={styles.signInMenu}>
-          <Pressable style={styles.nextButton} onPress={() => navigation.navigate('EmailSignIn1')}>
+          <Pressable style={styles.nextButton} onPress={checkTextInput}>
             <Text style={styles.buttonText}>Next</Text>
+            
           </Pressable>
+          <Button style={styles.backButton} title="Go back" onPress={() => navigation.navigate('MainLoginScreen')} />
         </View>
 
       </ImageBackground>
@@ -66,23 +96,17 @@ const styles = StyleSheet.create({
     height: '100%',
 
   },
-  header: {
-    height: 50,
-    width: '100%',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
   infoMenu: {
-    height: 500,
+    height: 350,
     marginRight: 40,
     marginLeft: 40,
-    marginBottom: 40,
-
-    padding: 50,
+    marginTop: 75,
+    padding: 30,
     alignItems: 'center',
     justifyContent: 'center',
-    flexDirection: 'column',
+    textAlign: 'center',
+    borderRadius: 20,
 
   },
   signInMenu: {
@@ -121,6 +145,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: '#73172F',
     marginTop: 20,
+    marginBottom: 20,
   },
 
 
@@ -139,6 +164,7 @@ const styles = StyleSheet.create({
     lineHeight: 21,
     fontWeight: 'bold',
     color: 'white',
+
   },
 
 
