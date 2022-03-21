f8c929e455eb0e7cdf920fa0cc43fcf5f2c04715
John Rey <42550367+SirPancakes@users.noreply.github.com>
2022-03-20 23:57:09 -0700

    cleaner profile sidebar

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Dashboard from './screens/Dashboard'
-import YourProfile from './screens/YourProfile'
 
 const Stack = createNativeStackNavigator();
 
@@ -16,15 +15,6 @@ export default function App() {
 				component={Dashboard}
 				options={{ title: 'Dashboard', headerShown:false }}
 			/>
-
-			<Stack.Screen
-				name="YourProfile"
-				component={YourProfile}
-				options={{ title: 'YourProfile', headerShown:true }}
-			/>		
-
-
-
 		</Stack.Navigator>
 	</NavigationContainer>
   );
--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,55 +1,102 @@
-import React, {useState} from 'react'
-import { Text, View, StyleSheet, Button, Image, TouchableOpacity,  } from 'react-native';
+import React, { useState } from 'react'
+import { Text, View, StyleSheet, Button, Image, TouchableOpacity, } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { t } from 'react-native-tailwindcss';
+import ProfilePicture from 'react-native-profile-picture'
 
 
 const Dashboard = ({ navigation }) => {
 
+	const [show, setShow] = useState(false);
 
-const [show, setShow] = useState(false);
+	return (
+		<View style={styles.container}>
+
+			{show && (
+
+				<View style={styles.profile}>
 
 
-  return (
-    <View style={styles.container}>
+					<TouchableOpacity style={{ alignSelf: 'flex-start', marginLeft: 8, }}
+						onPress={
+							() => { setShow(!show) }
+						}>
+
+						<View style={styles.menuLogoContainer}>
+							<Image style={styles.menuLogo} source={require('../assets/hamburger_icon.png')} />
+						</View>
+
+					</TouchableOpacity>
+
+					<View style={{ marginTop: 18, alignItems: 'center' }}>
+						<ProfilePicture
+							width={75}
+							height={75}
+							requirePicture={require('../assets/avatar.jpg')}
+							isPicture={true}
+							user="RabidBlueberry"
+						/>
+
+
+						<View>
+							<Text style={styles.nameText}>RabidBlueberry</Text>
+						</View>
+
+						<View>
+							{/* Steam Status Here */}
+							<Text style={styles.statusText}>Online</Text>
+						</View>
+
+						<View style={{ alignItems: 'center', borderTopWidth: .5, borderTopColor: '#707070', width: '100%'}}>
+							<View>
+								<Text style={styles.platformsText}>Platforms</Text>
+							</View>
+						</View>
+
+						<View style={{ alignItems: 'center', borderTopWidth: .5, borderTopColor: '#707070'}}>
+							<View>
+								<Text style={styles.platformsText}>Friends</Text>
+							</View>
+							<View style={{ alignSelf: 'flex-start', flexDirection: 'row', margin: 20 }}>
+								<ProfilePicture
+									width={50}
+									height={50}
+									requirePicture={require('../assets/avatar.jpg')}
+									isPicture={true}
+									user="RabidBlueberry"
+								/>
+								<Text style={styles.nameText}>   SirPancakes</Text>
+							</View>
+						</View>
+
+
+					</View>
+
+				</View>)}
+
 
 			{/* HEADER */}
 			<View style={styles.header}>
-        
-        <Image style={styles.logo} source={require('../assets/playgg_logo_concept.png')} />
+
+				<Image style={styles.logo} source={require('../assets/playgg_logo_concept.png')} />
 
 
-        <TouchableOpacity 
-        onPress = {
-          () => 
-          
-          {setShow(!show)}
+				<TouchableOpacity
+					onPress={
+						() => { setShow(!show) }
 
-          }>
+					}>
+					<View style={styles.menuLogoContainer}>
+						<Image style={styles.menuLogo} source={require('../assets/hamburger_icon.png')} />
+					</View>
+				</TouchableOpacity>
 
-            <Image style={styles.menuLogo} source={require('../assets/hamburger_icon.png')} />
+			</View>
 
-        </TouchableOpacity>
 
-			</View> 
+		</View>
 
-      {show && (
-        <View style={styles.profile}>
-          <Text style={styles.nameText}>RabidBlueberry</Text>
-
-          {/* Steam Status Here */}
-          <Text style={styles.statusText}>Online</Text>
-
-          <Text style={styles.platformsText}>Platforms</Text>
-
-          <Text style={styles.platformsText}>Friends</Text>
-
-         
-
-        </View>)}
-    </View>
-
-  )
+	)
 }
 
 
@@ -57,41 +104,49 @@ const [show, setShow] = useState(false);
 
 export default Dashboard
 
- const styles = StyleSheet.create({
-   container: {
-      height: '100%',
-      width: '100%',
-      backgroundColor: '#211626'
-   },
+const styles = StyleSheet.create({
+	container: {
+		height: '100%',
+		width: '100%',
+		backgroundColor: '#211626'
+	},
 
-   header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: '#73172f'
-   },
+	header: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		backgroundColor: '#73172f'
+	},
 
-   logo: {
-    width: 75,
-    height: 75,
-   },
+	logo: {
+		width: 75,
+		height: 75,
+	},
 
-   menuLogo: {
-    width: 75,
-    height: 75,
-    
-   },
+	menuLogo: {
+		width: 50,
+		height: 50,
+
+	},
+
+	menuLogoContainer: {
+		alignItems: 'center',
+		marginTop: 8,
+		marginRight: 8,
+	},
+
+	profile: {
+		alignSelf: 'flex-end',
+		alignItems: 'center',
+		backgroundColor: '#2b222a',
+		height: '100%',
+		width: '75%',
+		position: 'absolute',
+		zIndex: 2,
 
 
-   profile:{
-    alignSelf: 'flex-end',
-    alignItems:'center',
-    backgroundColor:'#2b222a',
-    height: '100%',
-    width: '75%',
+	},
 
-   },
-
-   nameText: {
+	nameText: {
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#D92344',
@@ -99,7 +154,7 @@ export default Dashboard
 		marginBottom: 4
 	},
 
-  statusText: {
+	statusText: {
 		fontSize: 20,
 		fontWeight: 'bold',
 		color: '#076600',
@@ -107,7 +162,7 @@ export default Dashboard
 		marginBottom: 4
 	},
 
-  platformsText: {
+	platformsText: {
 		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#D92344',
