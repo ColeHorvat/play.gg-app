195529c665855d0fd233284a52226797ed0b7fa3
Cole Horvat <colehorvat@pop-os.localdomain>
2022-03-27 19:01:47 +0000

    Added styles to friend request

--- screens/components/Dashboard/FriendRequest.js
+++ screens/components/Dashboard/FriendRequest.js
@@ -1,4 +1,4 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/Feather'
 import ProfilePicture from 'react-native-profile-picture'
@@ -19,7 +19,7 @@ function FriendRequest(props) {
 
 
     return (
-        <View style={{ backgroundColor: 'red', flexDirection: 'row' }}>
+        <View style={{ backgroundColor: 'rgba(33, 22, 38, 75)', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 10, borderRadius: 10 }}>
             <ProfilePicture
                 width={40}
                 height={40}
@@ -27,22 +27,98 @@ function FriendRequest(props) {
                 isPicture={true}
                 user="SirPancakes"
             />
-            <Text>Sirpancakes</Text>
-            <Icon.Button
-                name="check"
-                size={20}
-                color="green"
-                onPress={addFriend}
+            <Text style={[styles.nameText, {fontSize: 14, marginLeft: '3%'}]}>Sirpancakes</Text>
+            <View style={[{flexDirection: 'row', marginLeft: '3%'}]}>
+                <Icon.Button
+                    name="check"
+                    size={20}
+                    color="green"
+                    backgroundColor="rgba(33, 22, 38, 75)"
+                    onPress={addFriend}
 
-            />
-            <Icon.Button
-                name="x"
-                size={20}
-                color="red"
-                onPress={() => console.log("REJECT REQUEST")}
-            />
+                />
+                <Icon.Button
+                    name="x"
+                    size={20}
+                    color="red"
+                    backgroundColor="rgba(33, 22, 38, 75)"
+                    onPress={() => console.log("REJECT REQUEST")}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+	container: {
+		height: '100%',
+		width: '100%',
+		backgroundColor: '#211626'
+	},
+
+	logo: {
+		width: 75,
+		height: 75,
+	},
+
+	menuLogo: {
+		width: 50,
+		height: 50,
+
+	},
+
+
+	menuLogoContainer: {
+		alignItems: 'center',
+		marginTop: 8,
+		marginRight: 8,
+	},
+
+	container: {
+		height: '100%',
+		width: '100%',
+		backgroundColor: '#211626'
+	},
+
+	headerNameText: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#D92344',
+		marginTop: 4,
+		marginBottom: 4
+	},
+
+	statusText: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		color: '#076600',
+		marginTop: 2,
+		marginBottom: 4
+	},
+
+	platformsText: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#D92344',
+		marginTop: 10,
+		marginBottom: 10
+	},
+	cardContainer: {
+		height: 150,
+		width: '100%',
+		backgroundColor: '#73172F',
+		marginVertical: 24,
+		flexDirection: 'row',
+	},
+	nameText: {
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#D92344',
+		marginTop: 4,
+		marginBottom: 4
+	},
+
+});
+
 
 export default FriendRequest
